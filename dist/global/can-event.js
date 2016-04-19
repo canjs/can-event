183ce6a908f88c0f5840ac094c57c15813195228
David Luecke <daff@neyeon.com>
2016-04-19 08:29:51 -0700

    3.0.0-pre.1

---
+++ dist/global/can-event.js
@@ -0,0 +1,240 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		modules[moduleName] = module && module.exports ? module.exports : result;
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-event@3.0.0-pre.0#can-event*/
+define('can-event', function (require, exports, module) {
+    var domEvents = require('can-util/dom/events/events');
+    var CID = require('can-util/js/cid/cid');
+    var isEmptyObject = require('can-util/js/is-empty-object/is-empty-object');
+    var domDispatch = require('can-util/dom/dispatch/dispatch');
+    var canEvent = {
+        addEventListener: function (event, handler) {
+            var allEvents = this.__bindEvents || (this.__bindEvents = {}), eventList = allEvents[event] || (allEvents[event] = []);
+            eventList.push({
+                handler: handler,
+                name: event
+            });
+            return this;
+        },
+        removeEventListener: function (event, fn, __validate) {
+            if (!this.__bindEvents) {
+                return this;
+            }
+            var events = this.__bindEvents[event] || [], i = 0, ev, isFunction = typeof fn === 'function';
+            while (i < events.length) {
+                ev = events[i];
+                if (__validate ? __validate(ev, event, fn) : isFunction && ev.handler === fn || !isFunction && (ev.cid === fn || !fn)) {
+                    events.splice(i, 1);
+                } else {
+                    i++;
+                }
+            }
+            return this;
+        },
+        dispatch: function (event, args) {
+            var events = this.__bindEvents;
+            if (!events) {
+                return;
+            }
+            var eventName;
+            if (typeof event === 'string') {
+                eventName = event;
+                event = { type: event };
+            } else {
+                eventName = event.type;
+            }
+            var handlers = events[eventName];
+            if (!handlers) {
+                return;
+            } else {
+                handlers = handlers.slice(0);
+            }
+            var passed = [event];
+            if (args) {
+                passed.push.apply(passed, args);
+            }
+            for (var i = 0, len = handlers.length; i < len; i++) {
+                handlers[i].handler.apply(this, passed);
+            }
+            return event;
+        },
+        on: function (eventName, selector, handler) {
+            var listenWithDOM = domEvents.canAddEventListener.call(this);
+            var eventBinder = listenWithDOM ? domEvents : canEvent;
+            if (typeof selector === 'string') {
+                return eventBinder.addDelegateListener.apply(this, arguments);
+            } else {
+                return eventBinder.addEventListener.apply(this, arguments);
+            }
+        },
+        off: function (eventName, selector, handler) {
+            var listenWithDOM = domEvents.canAddEventListener.call(this);
+            var eventBinder = listenWithDOM ? domEvents : canEvent;
+            if (typeof selector === 'string') {
+                return eventBinder.removeDelegateListener.apply(this, arguments);
+            } else {
+                return eventBinder.removeEventListener.apply(this, arguments);
+            }
+        },
+        trigger: function () {
+            var listenWithDOM = domEvents.canAddEventListener.call(this);
+            var dispatch = listenWithDOM ? domDispatch : canEvent.dispatch;
+            return dispatch.apply(this, arguments);
+        },
+        one: function (event, handler) {
+            var one = function () {
+                canEvent.off.call(this, event, one);
+                return handler.apply(this, arguments);
+            };
+            canEvent.on.call(this, event, one);
+            return this;
+        },
+        listenTo: function (other, event, handler) {
+            var idedEvents = this.__listenToEvents;
+            if (!idedEvents) {
+                idedEvents = this.__listenToEvents = {};
+            }
+            var otherId = CID(other);
+            var othersEvents = idedEvents[otherId];
+            if (!othersEvents) {
+                othersEvents = idedEvents[otherId] = {
+                    obj: other,
+                    events: {}
+                };
+            }
+            var eventsEvents = othersEvents.events[event];
+            if (!eventsEvents) {
+                eventsEvents = othersEvents.events[event] = [];
+            }
+            eventsEvents.push(handler);
+            canEvent.addEventListener.call(other, event, handler);
+        },
+        stopListening: function (other, event, handler) {
+            var idedEvents = this.__listenToEvents, iterIdedEvents = idedEvents, i = 0;
+            if (!idedEvents) {
+                return this;
+            }
+            if (other) {
+                var othercid = CID(other);
+                (iterIdedEvents = {})[othercid] = idedEvents[othercid];
+                if (!idedEvents[othercid]) {
+                    return this;
+                }
+            }
+            for (var cid in iterIdedEvents) {
+                var othersEvents = iterIdedEvents[cid], eventsEvents;
+                other = idedEvents[cid].obj;
+                if (!event) {
+                    eventsEvents = othersEvents.events;
+                } else {
+                    (eventsEvents = {})[event] = othersEvents.events[event];
+                }
+                for (var eventName in eventsEvents) {
+                    var handlers = eventsEvents[eventName] || [];
+                    i = 0;
+                    while (i < handlers.length) {
+                        if (handler && handler === handlers[i] || !handler) {
+                            canEvent.removeEventListener.call(other, eventName, handlers[i]);
+                            handlers.splice(i, 1);
+                        } else {
+                            i++;
+                        }
+                    }
+                    if (!handlers.length) {
+                        delete othersEvents.events[eventName];
+                    }
+                }
+                if (isEmptyObject(othersEvents.events)) {
+                    delete idedEvents[cid];
+                }
+            }
+            return this;
+        }
+    };
+    canEvent.bind = canEvent.addEventListener;
+    canEvent.addEvent = canEvent.addEventListener;
+    canEvent.unbind = canEvent.removeEventListener;
+    canEvent.removeEvent = canEvent.removeEventListener;
+    canEvent.delegate = canEvent.on;
+    canEvent.undelegate = canEvent.off;
+    module.exports = canEvent;
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
